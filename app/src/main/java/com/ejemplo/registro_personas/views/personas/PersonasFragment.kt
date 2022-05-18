ab225b745622c8a23b7754a974a5dc0a6d603b12
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-18 16:54:52 -0400

    programando boton guardar

--- app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasFragment.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasFragment.kt
@@ -3,32 +3,48 @@ package com.ejemplo.registro_personas.views.personas
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.ejemplo.registro_personas.R
+import com.ejemplo.registro_personas.databinding.PersonasFragmentBinding
+import com.ejemplo.registro_personas.model.Persona
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PersonasFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PersonasFragment()
-    }
 
-    private lateinit var viewModel: PersonasViewModel
+    private val viewModel: PersonasViewModel by viewModels()
+    private lateinit var binding: PersonasFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.personas_fragment, container, false)
+
+        binding = PersonasFragmentBinding.inflate(inflater, container, false)
+
+        binding.guardarButton.setOnClickListener {
+
+            val persona = Persona (0, binding.nombresEditText.text.toString(),
+            binding.emailEditText.text.toString(),
+            binding.ocupacionEditText.text.toString(),
+            binding.salarioEditText.floatValue()
+            )
+            viewModel.guardar(persona)
+
+            val snackbar = Snackbar.make(binding.root, "Persona Guardada", Snackbar.LENGTH_LONG)
+            snackbar.show()
+        }
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PersonasViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    fun TextInputEditText.floatValue() = text.toString().toFloatOrNull() ?: 0.0f
 
 }
--- app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasViewModel.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasViewModel.kt
@@ -1,9 +1,21 @@
 package com.ejemplo.registro_personas.views.personas
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ejemplo.registro_personas.data.PersonaDao
+import com.ejemplo.registro_personas.model.Persona
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class PersonasViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class PersonasViewModel @Inject constructor(
+    val personaDao: PersonaDao
+) : ViewModel() {
+
+    fun guardar(persona: Persona){
+        viewModelScope.launch {
+            personaDao.Insertar(persona)
+        }
+    }
 }
