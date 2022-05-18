30e4257c2449ba518797f63bcda812c7d515157d
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-18 10:42:10 -0400

    Programando el binding de ListaPersonasFragment

--- app/src/main/java/com/ejemplo/registro_personas/views/personas/ListaPersonasFragment.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/ListaPersonasFragment.kt
@@ -6,20 +6,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ejemplo.registro_personas.R
+import com.ejemplo.registro_personas.databinding.ListaPersonasFragmentBinding
 
 class ListaPersonasFragment : Fragment() {
 
    // private lateinit var viewModel: ListaPersonasViewModel
+    private lateinit var binding: ListaPersonasFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
+        binding = ListaPersonasFragmentBinding.inflate(inflater, container, false)
 
+        binding.personaButton.setOnClickListener {
+            findNavController().navigate(R.id.action_to_personasFragment)
+        }
 
-        return inflater.inflate(R.layout.lista_personas_fragment, container, false)
+        return binding.root
     }
 
 
