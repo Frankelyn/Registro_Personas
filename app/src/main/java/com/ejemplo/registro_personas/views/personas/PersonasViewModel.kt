e4da0d51f446829eb33fbd427fdba2613b0b8bf7
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-18 17:35:27 -0400

    modificando PersonaViewModel

--- app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasViewModel.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.ejemplo.registro_personas.data.PersonaDao
 import com.ejemplo.registro_personas.model.Persona
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -12,6 +13,9 @@ import javax.inject.Inject
 class PersonasViewModel @Inject constructor(
     val personaDao: PersonaDao
 ) : ViewModel() {
+
+    val personas: Flow<List<Persona>>
+        get() = personaDao.GetList()
 
     fun guardar(persona: Persona){
         viewModelScope.launch {
