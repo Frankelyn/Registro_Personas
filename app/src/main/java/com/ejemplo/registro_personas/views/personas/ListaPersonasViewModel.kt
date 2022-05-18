d593fa394dc9072354cfd082d79e487933382cb6
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-18 11:20:25 -0400

    agregando @HiltViewModel

--- app/src/main/java/com/ejemplo/registro_personas/views/personas/ListaPersonasViewModel.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/ListaPersonasViewModel.kt
@@ -1,7 +1,9 @@
 package com.ejemplo.registro_personas.views.personas
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class ListaPersonasViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
--- app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasViewModel.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasViewModel.kt
@@ -1,7 +1,9 @@
 package com.ejemplo.registro_personas.views.personas
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class PersonasViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
