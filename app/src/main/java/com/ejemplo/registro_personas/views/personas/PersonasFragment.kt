8ce0fc5035e8762046b995b39a4c4581bddc8fc2
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-18 17:52:05 -0400

    modificando PersonasFragment

--- app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasFragment.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/PersonasFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.ejemplo.registro_personas.R
 import com.ejemplo.registro_personas.databinding.PersonasFragmentBinding
 import com.ejemplo.registro_personas.model.Persona
@@ -22,6 +23,11 @@ class PersonasFragment : Fragment() {
     private val viewModel: PersonasViewModel by viewModels()
     private lateinit var binding: PersonasFragmentBinding
 
+    //Atrapar la profesion pasada por argumento
+    private val args: PersonasFragmentArgs by navArgs()
+
+    private var personaId: Int = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,6 +35,7 @@ class PersonasFragment : Fragment() {
 
         binding = PersonasFragmentBinding.inflate(inflater, container, false)
 
+        llenaCampos()
         binding.guardarButton.setOnClickListener {
 
             val persona = Persona (0, binding.nombresEditText.text.toString(),
@@ -43,6 +50,19 @@ class PersonasFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun llenaCampos() {
+        val persona: Persona? = args.persona
+        persona?.let {
+            personaId = it.personaId
+            binding.nombresEditText.setText(it.nombres)
+            binding.emailEditText.setText(it.email)
+            binding.ocupacionEditText.setText(it.ocupacion)
+            binding.salarioEditText.setText(it.salario.toString())
+
+
+        }
     }
 
     fun TextInputEditText.floatValue() = text.toString().toFloatOrNull() ?: 0.0f
