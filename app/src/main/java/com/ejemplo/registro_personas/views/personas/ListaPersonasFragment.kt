899e4562419ea195c30a826af32e90c19eee165c
Frankelyn <Frankelyn_garcia@ucne.edu.do>
2022-05-18 10:35:52 -0400

    agregando el viewBinding al Gradle

--- app/src/main/java/com/ejemplo/registro_personas/views/personas/ListaPersonasFragment.kt
+++ app/src/main/java/com/ejemplo/registro_personas/views/personas/ListaPersonasFragment.kt
@@ -10,23 +10,18 @@ import com.ejemplo.registro_personas.R
 
 class ListaPersonasFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ListaPersonasFragment()
-    }
-
-    private lateinit var viewModel: ListaPersonasViewModel
+   // private lateinit var viewModel: ListaPersonasViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+
         return inflater.inflate(R.layout.lista_personas_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ListaPersonasViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
